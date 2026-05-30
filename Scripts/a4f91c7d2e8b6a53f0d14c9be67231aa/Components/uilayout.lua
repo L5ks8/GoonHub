5601690fddf03bb8368edab6d06db43b53fe5dae
L5ks8 <lukebarz10@gmail.com>
2026-05-30 04:59:40 +0200

    Update uilayout.lua

--- Scripts/a4f91c7d2e8b6a53f0d14c9be67231aa/Components/uilayout.lua
+++ Scripts/a4f91c7d2e8b6a53f0d14c9be67231aa/Components/uilayout.lua
@@ -5,13 +5,21 @@ local UIFunctions = GoonHub.Import("Assets/Components/uifunctions")
 local UILayout = {}
 
 function UILayout.Create()
-    local Window = UI:MakeWindow({
-        Name = "GoonHub",
-        HidePremium = false,
-        SaveConfig = true,
-        ConfigFolder = "GoonHub"
-    })
-    return Window
-end
 
+    getgenv().NyroxToggleStates = getgenv().NyroxToggleStates or {}
+
+
+    local G2L = UI.CreateBase("GoonHub", "1.0.0")
+    local window = {
+        TabCount = 0, 
+        Stats = {
+            FPS = G2L["fps_label"],
+            Ping = G2L["ping_label"],
+            Memory = G2L["mem_label"]
+        }
+    }
+
+    Widgets.Init(window, G2L)
+
+    UIFunctions.Init(G2L, window)
 return UILayout
