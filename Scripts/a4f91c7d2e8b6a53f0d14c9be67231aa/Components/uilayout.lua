c6dfba46a0fe0c568d856fa36018f45b95509065
L5ks8 <lukebarz10@gmail.com>
2026-05-30 04:44:59 +0200

    Add files via upload

--- Scripts/a4f91c7d2e8b6a53f0d14c9be67231aa/Components/uilayout.lua
+++ Scripts/a4f91c7d2e8b6a53f0d14c9be67231aa/Components/uilayout.lua
@@ -1,3 +1,17 @@
 local UI = GoonHub.Import("Assets/ui")
 local Widgets = GoonHub.Import("Assets/Components/widgets")
 local UIFunctions = GoonHub.Import("Assets/Components/uifunctions")
+
+local UILayout = {}
+
+function UILayout.Create()
+    local Window = UI:MakeWindow({
+        Name = "GoonHub",
+        HidePremium = false,
+        SaveConfig = true,
+        ConfigFolder = "GoonHub"
+    })
+    return Window
+end
+
+return UILayout
